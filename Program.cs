b4867df2f702ff5faf217d98829de257ceba6346
Amirreza Tavakoli <60503794+amirrezatav@users.noreply.github.com>
2021-06-18 21:21:54 +0430

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Table
         static void Main()
         {
             Table table = new Table(50,ConsoleColor.Gray,ConsoleColor.Black);
-            table.AddRow("Title 1", "Title 2", "Title 3", "Title n");
+            table.AddRow("Title 1", "Title 2", "Title 3", "Title n" /*....... args*/);
             table.AddRow("Title 1", "Title 2", "Title 3", "Title n");
             table.AddRow("Title 1", "Title 2", "Title 3", "Title n");
             table.AddRow("Title 1", "Title 2", "Title 3", "Title n");
